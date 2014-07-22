0e538e242c44108c97cbbefbd35319ecaa6ce47b
Yuji Fukumori <Yuji@Yujis-MacBook-Pro.local>
2014-07-22 16:32:12 -0400

    First commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  def forem_name
+  	email
+  end
+
+  def forem_email
+  	email_address
+  end       
+end
