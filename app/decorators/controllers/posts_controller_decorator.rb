3d9b7eb12ff8c702acc6e2392afc54142e1545ef
theoandtheb <blue.apple@sympatico.ca>
2014-07-28 11:57:35 -0400

    Finally resolved the host domain problem. Migrated the rest of the post controller to the posts decorator.

--- app/decorators/controllers/posts_controller_decorator.rb
+++ app/decorators/controllers/posts_controller_decorator.rb
@@ -22,10 +22,38 @@ Forem::PostsController.class_eval do
       end
     end
 
+    def edit
+    end
+
+    def update
+      if @post.owner_or_admin?(forem_user) && @post.update_attributes(post_params)
+        update_successful
+      else
+        update_failed
+      end
+    end
+
+    def destroy
+      @post.destroy
+      destroy_successful
+    end
+
 private
 
         def post_params
       params.require(:post).permit(:text, :reply_to_id)
+    end
+
+        def authorize_reply_for_topic!
+      authorize! :reply, @topic
+    end
+
+        def authorize_edit_post_for_forum!
+      authorize! :edit_post, @topic.forum
+    end
+
+    def authorize_destroy_post_for_forum!
+      authorize! :destroy_post, @topic.forum
     end
 
     def find_reply_to_post
@@ -41,5 +69,55 @@ private
       params[:reply_to_id] = params[:post][:reply_to_id]
       flash.now.alert = t("forem.post.not_created")
       render :action => "new"
+    end
+
+        def destroy_successful
+      if @post.topic.posts.count == 0
+        @post.topic.destroy
+        flash[:notice] = t("forem.post.deleted_with_topic")
+        redirect_to [@topic.forum]
+      else
+        flash[:notice] = t("forem.post.deleted")
+        redirect_to [@topic.forum, @topic]
+      end
+    end
+
+    def update_successful
+      redirect_to [@topic.forum, @topic], :notice => t('edited', :scope => 'forem.post')
+    end
+
+    def update_failed
+      flash.now.alert = t("forem.post.not_edited")
+      render :action => "edit"
+    end
+
+    def ensure_post_ownership!
+      unless @post.owner_or_admin? forem_user
+        flash[:alert] = t("forem.post.cannot_delete")
+        redirect_to [@topic.forum, @topic] and return
+      end
+    end
+
+    def find_topic
+      @topic = Forem::Topic.friendly.find params[:topic_id]
+    end
+
+    def find_post_for_topic
+      @post = @topic.posts.find params[:id]
+    end
+
+    def block_spammers
+      if forem_user.forem_spammer?
+        flash[:alert] = t('forem.general.flagged_for_spam') + ' ' +
+                        t('forem.general.cannot_create_post')
+        redirect_to :back
+      end
+    end
+
+    def reject_locked_topic!
+      if @topic.locked?
+        flash.alert = t("forem.post.not_created_topic_locked")
+        redirect_to [@topic.forum, @topic] and return
+      end
     end
 end
