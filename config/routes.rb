e09373c403a7134fdba167d825fb352c919391ab
Matthew <wong.mat@gmail.com>
2014-07-29 14:02:22 -0400

    slight edits to sheets

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :sheets
+
   root to: 'pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
