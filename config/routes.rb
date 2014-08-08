5991083200c2e42f146d9d659c5c9aa0db1823fe
theoandtheb <blue.apple@sympatico.ca>
2014-08-08 09:47:11 -0400

    Removed excess and unused resources from routes, eliminated the floating body glitch from the home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,6 @@
-# Forem::Engine.routes.draw do
-#   resources :forums, :only => [:new, :update, :edit, :destroy, :create, :show], :path => "/" do
-#     resources :topics
-#   end 
-# end
-
-
-
-#link from the forum to this URL: /sheets/new?user_id=12&forum_id=13&topic_id=14
-#These will end up in params[:forum_id]
-# If someone tries to visit the /sheets/2 URL and are not the right user, redirect to root
-#Create SheetsController under actual controllers folder
-#Profit
-
 Rails.application.routes.draw do
 
 
-  # This line mounts Forem's routes at /forums by default.
-  # This means, any requests to the /forums URL of your application will go to Forem::ForumsController#index.
-  # If you would like to change where this extension is mounted, simply change the :at option to something different.
-  #
-  # We ask that you don't use the :as option here, as Forem relies on it being the default of "forem"
   mount Forem::Engine, :at => '/forums'
 
   devise_for :users
@@ -27,59 +8,5 @@ Rails.application.routes.draw do
   resources :sheets
 
   root to: 'pages#home'
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
