11803bdf44dceeb204cba27b0ce84d664b1e7c20
theoandtheb <blue.apple@sympatico.ca>
2014-08-17 11:11:27 -0400

    Added comments and patched indentation on sheets_controller.

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -3,14 +3,14 @@ class SheetsController < ApplicationController
 
   def new
       @sheet = @topic.sheets.build
+      #As sheets are outside of the Forem engine, the forum_id does not pass through as a resource.
       @sheet.forum_id = @topic.forum.id
   end
 
   def create
-      # @topic = Forem::Topic.find(params(:topic_id))
-      # @sheet = @topic.sheets.build(sheet_params)
 
       @sheet = Sheet.new(sheet_params)
+      #As sheets are outside of the Forem engine, the user does not pass through as a resource.
       @sheet.user = forem_user
 
       if @sheet.save
@@ -21,14 +21,18 @@ class SheetsController < ApplicationController
   end
 
   def edit
+    #Allows for params embedded in the URL to be used to trace the sheet matching the oriignating topic and user.
     @sheet = Sheet.where(topic_id: params[:topic_id], user_id: forem_user.id).first
+    #Likely overkill, may be worth removing during refactoring.
     @sheet.forum_id = params[:forum_id]
   end
 
     def update
+      #Allows for params embedded in the URL to be used to trace the sheet matching the oriignating topic and user.
       @sheet = Sheet.where(topic_id: params[:sheet][:topic_id], user_id: forem_user.id).first
       @sheet.update_attributes(sheet_params)
-        redirect_to forem.forum_topic_url(:action => "show", :contorller => "forem/topics", :format => nil, :forum_id => @sheet.forum_id, :id => @sheet.topic_id)
+      #As there is no association between the sheet and the topic or user, these values are passed on through the URL to allow for the user to re-enter the engine
+      redirect_to forem.forum_topic_url(:action => "show", :contorller => "forem/topics", :format => nil, :forum_id => @sheet.forum_id, :id => @sheet.topic_id)
   end
 
   def destroy
@@ -44,12 +48,12 @@ class SheetsController < ApplicationController
     end
 
     def find_topic
-      # @topic = Topic.friendly.find params[:topic_id]
       @topic = Forem::Topic.find(params[:topic_id])
     end
 
     def create_successful
       flash[:notice] = "Your character sheet has been successfully saved."
+      #As there is no association between the sheet and the topic or user, these values are passed on through the URL to allow for the user to re-enter the engine
       redirect_to forem.forum_topic_url(:action => "show", :contorller => "forem/topics", :format => nil, :forum_id => @sheet.forum_id, :id => @sheet.topic_id)
     end
 
@@ -59,7 +63,8 @@ class SheetsController < ApplicationController
       render :action => "new"
     end
 
-        def update_successful
+    def update_successful
+      #As there is no association between the sheet and the topic or user, these values are passed on through the URL to allow for the user to re-enter the engine
       redirect_to forem.forum_topic_url(:action => "show", :contorller => "forem/topics", :format => nil, :forum_id => @sheet.forum_id, :id => @sheet.topic_id)
     end
 
