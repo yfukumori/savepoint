a2c43e9822271d2cdb024a372192c5a8a9cddde6
theoandtheb <blue.apple@sympatico.ca>
2014-08-17 11:24:35 -0400

    Comment added to topic_decorator model.

--- app/decorators/models/forem/topic_decorator.rb
+++ app/decorators/models/forem/topic_decorator.rb
@@ -1,4 +1,5 @@
 Forem::Topic.class_eval do
 	has_many :sheets
+	#Decorator added for the sole purpose of changing the display order from ascending to decending.
 	 has_many   :posts, -> { order "forem_posts.created_at DESC"}, :dependent => :destroy
 end
